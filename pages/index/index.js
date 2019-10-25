0276895c83422b54bcabcb110857638c01797adc
QingboTian <tqb820965236@163.com>
2019-10-26 00:25:10 +0800

    commit

---
+++ pages/index/index.js
@@ -0,0 +1,123 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data : {
+    images: [// 准备展示的图片
+      { url: "../../images/4.jpg", height: 313, width: 500 },
+      { url: "../../images/5.jpeg", height: 2560, width: 1440 },
+      { url: "../../images/6.jpeg", height: 1216, width: 700 },
+      { url: "../../images/1.jpg", height: 1200, width: 1920 },
+      { url: "../../images/2.jpg", height: 1200, width: 1920 },
+      { url: "../../images/3.jpg", height: 300, width: 533 },
+      { url: "../../images/6.jpeg", height: 1216, width: 700 },
+    ],
+    leftShowImages: [],// 左边已经展示的图片
+    rightShowImages: [],// 右边已经展示的图片
+    leftHeight: 0,
+    rightHeight: 0,
+    index: 0// 已经加载图片的索引
+  },
+
+  onLoad() {
+    this.setData({
+      search: this.search.bind(this)
+    })
+
+    var length = this.data.images.length
+    for (var i = 0; i < length; i++) {
+      this.loadImage(this)
+    }
+
+    wx.setNavigationBarTitle({
+      title: '短视频',
+    })
+
+    // wx.hideShareMenu()
+  },
+
+  search: function (value) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve([{ text: '搜索结果', value: 1 }, { text: '搜索结果2', value: 2 }])
+      }, 200)
+      
+    })
+  },
+  selectResult: function (e) {
+    console.log('select result', e.detail)
+  },
+
+  // 更新版本（可根据容器中图片的实时高度（模拟）进行图片的添加）
+  loadImage: function (that) {
+    var leftHeight = this.data.leftHeight// 左容器高度
+    var rightHeight = this.data.rightHeight// 右容器高度
+    var index = this.data.index// 加载图片的索引
+    var images = this.data.images// 总共要加载的图片
+
+    // 这个值在不同的手机中是不同的，但是不影响整体的效果，因为最终的高度是按照每张图片的
+    // 长宽比进行计算，虽然不是容器的实时高度，但是左右容器高度的大小关系是可以计算出来的
+    var widthFix = 201;// 左（右）容器中图片的固定宽度（iPhone6P）
+
+    var min = Math.min(leftHeight, rightHeight)// 计算左右容器高度的最小值
+    // 添加要新加载的图
+    if (min == leftHeight) {
+      var leftShowImages = this.data.leftShowImages
+      leftShowImages.push(images[index])
+      that.setData({
+        leftShowImages: leftShowImages
+      })
+
+      // 计算当前容器内图片的高度
+      var currHeight = (widthFix * images[index].height) / images[index].width
+      // 获取图片高度
+      leftHeight += currHeight
+
+      // console.log("左高度" + leftHeight)
+
+    } else {
+      var rightShowImages = this.data.rightShowImages
+      rightShowImages.push(images[index])
+      that.setData({
+        rightShowImages: rightShowImages
+      })
+
+      // 按照长宽比计算的容器中图片应该的高
+      var currHeight = (widthFix * images[index].height) / images[index].width
+      // 获取图片高度
+      rightHeight += currHeight
+
+      // console.log("右高度" + rightHeight)
+
+    }
+
+    // 索引加1
+    index++;
+    that.setData({
+      index: index,
+      leftHeight: leftHeight,
+      rightHeight: rightHeight
+    })
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    this.setData({
+      index: 0
+    })
+
+    var length = this.data.images.length
+    for (var i = 0; i < length; i++) {
+      this.loadImage(this)
+    }
+  },
+
+  tapHandler : function () {
+    wx.navigateTo({
+      url: '../video/video',
+    })
+  }
+})
---
+++ pages/message/message.js
@@ -0,0 +1,139 @@
+// pages/message/message.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    flag : true, // 从本地缓存或者数据库读取是否存在消息列表
+    lists : [
+      {
+        poster: "../../images/message.jpeg",
+        name : "官方宝宝",
+        content : "欢迎加入我们！！！",
+        time : "12:00",
+        isRead : true// 已读
+      },
+      {
+        poster: "../../images/message.jpeg",
+        name: "亲爱的",
+        content: "现在在干什么呢？",
+        time: "13:00",
+        isRead: false// 未读
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: '消息',
+    })
+  },
+
+  // 长按删除对话列表
+  longTapHandler : function (e) {
+    // console.log(e.currentTarget.dataset.id)
+    // console.log("长按")
+
+    var that = this
+
+    wx.showModal({
+      title: '系统提示',
+      content: '是否删除对话',
+      success(res) {
+        if (res.confirm) {
+          var index = e.currentTarget.dataset.id
+          var lists = that.data.lists
+          lists.splice(index, 1)
+          that.setData({
+            lists: lists
+          })
+
+          // 判断数据是否删完
+          if(lists.length == 0) {
+            that.setData({
+              flag: false
+            })
+          }
+        }
+      }
+    })
+  },
+
+  // 点击事件
+  tapHandler : function (e) {
+
+    // console.log(e.currentTarget.dataset.id)
+    var index = e.currentTarget.dataset.id
+
+    // var isRead = this.data.lists[index].isRead
+    var lists = this.data.lists
+    var isRead = lists[index].isRead
+
+    if (!isRead){
+      // 若消息已读 则不修改状态 若消息未读 则修改状态为true
+      lists[index].isRead = true
+
+      this.setData({
+        lists: lists
+      })
+    }
+
+    wx.navigateTo({
+      // 跳转到聊天页面
+      url: '../chat/chat?id=' + index,
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
