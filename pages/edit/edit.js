968bc658bd85201f9d74a2dc8cbb1600bc48a34d
QingboTian <tqb820965236@163.com>
2019-10-28 23:52:35 +0800

    update

--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -1,11 +1,16 @@
-// pages/edit/edit.js
+// 引入util(获取系统时间))
+var util = require('../../utils/util.js');
+// 经纬度逆解析
+var bmap = require('../../libs/bmap-wx.min.js');
+var wxMarkerData = []; 
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    type : 3,
+    type : 7,
     titles: ["昵称", "个性签名", "手机", "邮箱", "性别", "生日", "地区"],
     nickWordLength : 0,
     nickname : "bobo",
@@ -13,7 +18,64 @@ Page({
     signature : "心若向阳，无畏悲伤！",
     checkBtnAble : false,
     checkNumStr: "发送验证码",
-    phone : "13888888888"
+    phone : "13888888888",
+    mailBefore : "",
+    mailLater : "",
+    index : 1,
+    array : ["火星人","男","女"],
+    date : "",
+    region: [],
+    customItem: '全部',
+    currentLocation : "",
+  },
+
+  // 性别 生日 地区picker改变值时触发的函数
+  bindPickerChange: function (e) {
+    // console.log('picker发送选择改变，携带值为', e.detail.value)
+    var data = {}
+    data[e.currentTarget.dataset.type] = e.detail.value
+    this.setData(data)
+  },
+
+  // 定位当前位置
+  switchLocation : function (e) {
+    var that = this
+
+    var BMap = new bmap.BMapWX({
+      ak: '7BHF4lj5zmEioDs9pSgTpoa0G7I6Bjoy' // 百度API AK
+    })
+
+    var fail = function (res) {
+      that.setData({
+        currentLocation: "遇到了一个错误,请检查网络是否连接"
+      })
+    }
+
+    BMap.regeocoding({
+      fail: fail,
+      success: function (res) {
+        // console.log(res.originalData.result.addressComponent)
+        var province = res.originalData.result.addressComponent.province
+        var city = res.originalData.result.addressComponent.city
+        var district = res.originalData.result.addressComponent.district // 区
+        var region = that.data.region
+        region.push(province)
+        region.push(city)
+        region.push(district)
+        that.setData({
+          currentLocation: province + city + district,
+          region: region // 这里应该获取的是用户的地区信息 若没有地区信息，应该设置为当前位置
+        })
+      }
+    });
+
+    // var that = this
+    // wx.getLocation({
+    //   success: function(res) {
+    //     console.log(res)
+    //     that.parse()
+    //   },
+    // })
   },
 
   /**
@@ -41,6 +103,19 @@ Page({
       nickWordLength: length1,
       signatureWordLength: length2
     })
+
+    // 获取当前日期
+    var datetime = util.formatTime(new Date());
+    var str = datetime.split(" ")
+    var date = str[0]
+    date = date.replace("/","-")
+    date = date.replace("/", "-")
+    this.setData({
+      date: date
+    })
+
+    // 获取位置
+    this.switchLocation()
   },
 
   // 判断字符长度
